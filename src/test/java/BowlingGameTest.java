a3744ee24fb59cfb4e20f647173b3ddd89f877e9
zkj <438101845@qq.com>
2020-08-04 01:16:54 +0800

    [zkj]-feat:error input of strike

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -24,6 +24,19 @@ public class BowlingGameTest {
 
     @Test
     void should_return_all_score_when_throw_given_correct_input() {
-        assertEquals(100, new BowlingGame().convert("1,9,2,8,3,7,10,2,8,5,5,5,5,5,5,5,5,5,5"));
+        assertEquals(100, new BowlingGame().convert("1,9,2,8,3,7,10,2,8,5,5,5,5,5,5,5,5,5,4"));
+    }
+
+    @Test
+    void should_return_all_score_when_throw_given_error_input_of_strike() {
+        assertThrows(RuntimeException.class, () -> {
+            new BowlingGame().convert("1,9,2,8,3,7,10,2,8,5,5,5,5,5,5,5,5,10,3,4,2");
+        });
+        assertThrows(RuntimeException.class, () -> {
+            new BowlingGame().convert("1,9,2,8,3,7,10,2,8,5,5,5,5,5,5,5,5,10,5,6");
+        });
+        assertThrows(RuntimeException.class, () -> {
+            new BowlingGame().convert("1,9,2,8,3,7,10,2,8,5,5,5,5,5,5,5,5,10,6");
+        });
     }
 }
