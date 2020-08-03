b78cf73ac55c90c440c363b45b5799e13034b53c
zkj <438101845@qq.com>
2020-08-03 13:28:53 +0800

    [zkj]-feat: when not knock all pins

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,8 @@
+public class BowlingGame {
+    public BowlingGame() {
+    }
+
+    public int convert(int i) {
+        return i;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingGameTest {
+    @Test
+    void should_return_origin_score_when_convert_given_number_of_pins_by_not_knock_all_pins() {
+        BowlingGame bowlingGame = new BowlingGame();
+
+        int numberPin = new Random().nextInt(10);
+        int result = bowlingGame.convert(numberPin);
+
+        assertEquals(numberPin, result);
+    }
+
+
 }
